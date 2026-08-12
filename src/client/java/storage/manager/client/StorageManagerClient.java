3d74f8e2023ceb2e8fc783002a2644e67c748a04
UwUtismXD <UwUtismXD@users.noreply.localhost>
2026-08-12 22:22:05 +0100

    Update from DESKTOP-3EPP80P at 2026-08-12T22:22:05Z

---
+++ src/client/java/storage/manager/client/StorageManagerClient.java
@@ -0,0 +1,10 @@
+package storage.manager.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StorageManagerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
